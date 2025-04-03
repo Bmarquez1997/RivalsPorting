4ca139e4adb8c781699597ddcd85b9efd76a4edc
Benjamin Marquez <bmarquez1997@gmail.com>
2025-04-03 07:12:38 -0400

    Tasty fixes, manifest update

--- FortnitePorting.Plugins/Blender/rivals_porting/__init__.py
+++ FortnitePorting.Plugins/Blender/rivals_porting/__init__.py
@@ -9,7 +9,7 @@ bl_info = {
     "description": "Import Server for Rivals Porting",
     "author": "Half, DeveloperChipmunk",
     "blender": (4, 2, 0),
-    "version": (0, 1, 5),
+    "version": (0, 1, 6),
     "category": "Import-Export",
 }
 
