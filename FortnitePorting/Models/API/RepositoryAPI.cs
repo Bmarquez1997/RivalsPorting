578c1ea0baba42ffddb8d6565fbfb95d372ae249
Benjamin Marquez <bmarquez1997@gmail.com>
2026-04-24 14:00:31 -0400

    Update repository and mappings URLs for new github raw format

--- FortnitePorting/Models/API/RepositoryAPI.cs
+++ FortnitePorting/Models/API/RepositoryAPI.cs
@@ -7,8 +7,8 @@ namespace FortnitePorting.Models.API;
 
 public class RepositoryAPI(RestClient client) : APIBase(client)
 {
-    public const string REPOSITORY_URL = "https://raw.githubusercontent.com/Bmarquez1997/RivalsPorting/refs/heads/RivalsPorting/Repository.json";
-    public const string MAPPINGS_URL = "https://raw.githubusercontent.com/Bmarquez1997/RivalsPorting/refs/heads/RivalsPorting/Mappings.json";
+    public const string REPOSITORY_URL = "https://github.com/Bmarquez1997/RivalsPorting/raw/refs/heads/main/Repository.json";
+    public const string MAPPINGS_URL = "https://github.com/Bmarquez1997/RivalsPorting/raw/refs/heads/main/Mappings.json";
     
     public async Task<RepositoryResponse?> GetRepositoryAsync(string url = REPOSITORY_URL)
     {
