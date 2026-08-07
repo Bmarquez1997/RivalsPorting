d07dd105bea6a21ae4723a7cf077120af62fd8c7
Benjamin Marquez <bmarquez1997@gmail.com>
2026-08-07 16:11:08 -0400

    refactor: Switch to SpaceDepot mappings API

--- RivalsPorting/Models/API/RepositoryAPI.cs
+++ RivalsPorting/Models/API/RepositoryAPI.cs
@@ -8,7 +8,7 @@ namespace RivalsPorting.Models.API;
 public class RepositoryAPI(RestClient client) : APIBase(client)
 {
     public const string REPOSITORY_URL = "https://raw.githubusercontent.com/Bmarquez1997/RivalsPorting/refs/heads/main/Repository.json";
-    public const string MAPPINGS_URL = "https://raw.githubusercontent.com/Bmarquez1997/RivalsPorting/refs/heads/main/Mappings.json";
+    public const string MAPPINGS_URL = "https://raw.githubusercontent.com/SpaceDepot/rivals-depot/refs/heads/main/Mappings.json";
     
     public async Task<RepositoryResponse?> GetRepositoryAsync(string url = REPOSITORY_URL)
     {
